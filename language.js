3c0bbabaab8928972cba50bfe27f6730a644d3f7
Jackieyewang <306981258@qq.com>
2021-11-17 02:29:30 +0800

    add chinese

--- language.js
+++ language.js
@@ -1,30 +1,84 @@
-// Translations object:
 var translations = {
-    'en': {
-      'home': 'Home',
-      'back': 'Back'
-      /* ... */
-    },
-    'zh': {
-      'home': 'Pradžia',
-      'back': 'Atgal'
-      /* ... */
-    }
-  };
-  
-  
-  // wait for all DOM elements to load
-  $(document).ready(function() {
-  
-    // when button is clicked
-    $('.lang-btn').click(function() {
-      // take translations subset
-      var lang = translations[$(this).data('lang')];
-  
-      // for each element that has "data-key" attribute
-      $('[data-key]').each(function() {
-  
-        // change it's content to other language
-        $(this).text(lang[$(this).data('key')]);
-      })
-    });
+  'en': {
+    'intro1':'Where Ethereum Builders Get Together',
+    'intro2':'togETHer is an Ethereum community alliance committed to driving the ecosystem foward. We connect community resources to collaborate on Ethereum blockchain documentation, development, research and events.',
+    'connect-btn':'CONTACT US',
+    'members':'Our Members.',
+    'intromember':'Together\'s current membership includes EThPlanet, ECN, EthFans, DAOSquare, PrimitivesLane, Hiblock, Zhixiong Pan, Rebase and BTCU.',
+    'introETHplanet':'ETHPlanet — Building a community to engage, connect, and empower builders. ETHPlanet, a planet where you can connect with all the developers, dapp founders, digital artists, investors, and other innovators in the field from around the Ethereum world.',
+    'introECN':'ECN as an Ethereum Community Network, is committed to facilitating the adoption of Ethereum and blockchain and further bridging the Chinese-speaking community with global Ethereum ecosystem. We’re making fundamental efforts including education, documentation, community and events.',
+    'introDaosquare':'About DAOSquare Born in the well-known West Ethereum community MetaCartel, DAOSquare is dedicated to building a Web3 incubator which aims to be the Y·Combinator in the Web3 era. ',
+    'introEthfans':'We focus on building the best Chinese Ethereum Community. We welcome collaborations from communities, projects around the world.',
+    'primitiveslane':'Primitives Lane',
+    'introprimitiveslane':'Primitives Lane (formerly known as Shanghai Advanced Tech Seminar) is a non-profit community focusing on cutting-edge technology, dedicated to exploring the development path of underlying technologies and researching fundamental public issues. By providing a public technology discussion platform for the industry through offline seminars and subject research, it promotes in-depth communication within the industry and enhances the influence of the industry in society. Currently, Primitives Lane focuses on blockchain, cryptography and other related areas.',
+    'introHiblock':'HiBlock is a developer community focused on blockchain, we not only promote blockchain among developers, but also help developers really master blockchain technology and applications.',
+    'introRebase':'Rebase community is committed to helping geeks make better use of technological innovation and promote social change.Rebase community project introduction: 1. Web3 Daily: Daily reading for geeks. A Web3 related technical article is published every day, and each article includes three recommendations. The recommended content comes from the members of the Web3 Daily Think Tank, and the content is collected, sorted out and published by community volunteers.	2. Rebase University: A place where geeks self-organized learning, advocating learning and sharing. Committed to building an open knowledge society. 3. Rostra: Inspired by Rebase University, Rostra is committed to changing the way people learn, research and share. Researchers publish research plans and raise funds from the society to fund their own research. The benefits of research results can benefit funders. 4. Dapp Learning: The project vision is to give junior developers an executable and simplest blockchain Dapp learning roadmap, and to give advanced developers a platform for communication and collaboration. The project is deeply integrated with Rebase University. A study group is set up to divide tasks and then study mainstream dapp projects together. Technology sharing is carried out every week. The sharing videos will be uploaded to the Rebase account at Bilibili. 5. Who is hiring: In order to facilitate the team\'s recruitment of developers, we setup a repo on github for freely publishing work requirements. 6. Rebase D. Ventures: Rebase D. Ventures is a community venture capital fund originating from the Rebase community. It uses funds from the community to realize the vision of helping and accomplishing blockchain technology teams and projects.',
+    'introBTCU':'The Blockchain Technology Community of Universities is composed of students from Tsinghua University, Peking University, Zhejiang University, Shanghai Jiaotong University, Fudan University, Renmin University of China, Central University of Finance and Economics, Wuhan University, Huazhong University of Science and Technology, Sichuan University, etc. The heads of the Blockchain Association of 20 Chinese universities jointly initiated the establishment of a cooperative community for blockchain technology learning, education and industrial communications among universities. It is a non-profit, unofficial, unincorporated, and spontaneous community.',
+    'Pan':'Zhixiong Pan',
+    'introPan':'ChainNews | Director of Research Observer of Ethereum/Layer2/Web3/DeFi.',
+    'events':'Events',
+    'introevents':'Highlight moments of togETHer',
+    'Hangzhou':'Hangzhou',
+    'Guangzhou':'Guangzhou',
+    'Shengzhen':'Shengzhen',
+    'Xiamen':'Xiamen',
+    'Chengdu':'Chengdu',
+    'join':'Join Us',
+    'how2join':'HOW TO BECOME ONE OF US',
+    'introJoin':'Ethereum contributors (community/individuals) are embraced by togETHer.',
+    'wanna0':'Wanna join us?',
+    'submit0':'Submit your proposal by Github issue',
+
+
+    /* ... */
+  },
+  'zh':  {
+    'intro1':'以太坊社区建设者团结的地方',
+    'intro2':'togETHer是一个致力于推动以太坊生态向前发展的以太坊社区联盟。我们连接社区资源，一起合协作以太坊区文档、开发、研究和活动。',
+    'connect-btn':'联系我们',
+    'members':'我们的成员',
+    'intromember':'Together的成员包括EThPlanet, ECN, EthFans, DAOSquare, PrimitivesLane, Hiblock, Zhixiong Pan, Rebase and BTCU.',
+    'introETHplanet':'ETHPlanet 以太行星 — 共建、互助、互联，赋能以太坊开发者社群ETHPlanet是一颗汇聚来自世界各地的以太坊翘楚的神奇星球，在这颗星球上，您可以与开发者、dapp 创始人、数字艺术家、投资者以及其他领域的创新者进行联系。我们携手支持并推进以太坊生态系统的开发。',
+    'introECN':'ECN 作为以太坊社区网络，旨在以社区化合作方式推动以太坊和区块链应用，进一步桥接华语社区和全球以太坊生态。我们为以太坊爱好者和开发者提供全面的教育支持、优质的前沿资讯和开放的社区平台。',
+    'introDaosquare':'DAOSquare 起源于西方著名以太坊社区 MetaCartel，DAOSquare 的使命是构建属于 Web3 时代的孵化器，从而更好地帮助这个时代的创新者从 0 到 1 并走向成功，正如互联网时代的 Y·Combinator 一样。 ',
+    'introEthfans':'以太坊爱好者致力于建立最好的以太坊中文技术社区。',
+    'primitiveslane':'原语里弄社区',
+    'introprimitiveslane':'原语里弄是聚焦前沿技术发展的非营利社区，致力于探索底层技术的发展路径，进行基础公共课题研究及应用。通过线下研讨、课题研究等形式，为行业提供公共技术讨论平台，推动行业内深度交流，并提高行业在社会中的影响力。',
+    'introHiblock':'HiBlock是一个专注于区块链的开发者社区，我们不仅仅在开发者中宣传推广区块链，还会帮助开发者真正掌握区块链技术与应用。',
+    'introRebase':'Rebase社区致力于帮助极客更好地利用科技创新，推动社会变革。Rebase社区项目/栏目介绍: 1. Web3极客日报: 极客们的日常读物。每天发布一篇Web3相关的技术文章，每篇文章包括三个推荐。推荐内容来自于Web3极客日报智囊团成员，由社区志愿者搜集内容并整理发出。2. Rebase大学: 极客们自组织学习的地方，倡导学习和分享。致力于建设开放知识的社会。3. Rostra: 受 Rebase大学启发，Rostra 致力于改变人们学习、研究和分享的方式，研究者发布研究计划并向社会募资，资助自身的研究，研究成果带来的收益可以让资助人受益。4. Dapp Learning: 项目愿景是给初级开发者一个可执行且最简的区块链 Dapp 学习路线图，给进阶开发者一个可以交流和协作的平台。项目跟Rebase大学深度结合，成立学习小组进行任务分工然后一起研究主流dapp项目，每周进行技术分享，输出成果会作为视频上传B站的Rebase账号。5. Who is hiring: 为方便团队招募开发者，在 github 上建立了一个自由发布工作需求的 repo。6. Rebase D. Ventures: Rebase D. Ventures 是发源于 Rebase 社区的社区风险投资基金，用来源于社区的资金来实现帮助和成就区块链技术团队、项目的愿景。',
+    'introBTCU':'高校区块链技术社区（Blockchain Technology Community of Universities）由来自清华大学、北京大学、浙江大学、上海交通大学、复旦大学、中国人民大学、中央财经大学、武汉大学、华中科技大学、四川大学等近 20 所中国高校的区块链协会负责人联合发起，意在组建高校间区块链技术学习、教育与产业交流的合作社区，属于非营利性、非官方、非法人性质、自发性的联合交流社群。',
+    'Pan':'潘致雄',
+    'introPan':'链闻 | 研究总监关注以太坊生态、Layer2、泛Web3和DeFi应用等领域。',
+    'events':'往期活动',
+    'introevents':'togETHer的活动留影',
+    'Hangzhou':'杭州',
+    'Guangzhou':'广州',
+    'Shengzhen':'深圳',
+    'Xiamen':'厦门',
+    'Chengdu':'成都',
+    'join':'加入我们',
+    'how2join':'如何成为我们的一员',
+    'introJoin':'together欢迎每一个以太坊贡献者(社区/个人)。',
+    'wanna0':'想加入我们吗?',
+    'submit0':'提交你的proposal在 Github issue',
+    /* ... */
+  }
+};
+
+
+// wait for all DOM elements to load
+$(document).ready(function() {
+
+  // when button is clicked
+  $('.lang-btn').click(function() {
+    // take translations subset
+    var lang = translations[$(this).data('lang')];
+
+    // for each element that has "data-key" attribute
+    $('[data-key]').each(function() {
+
+      // change it's content to other language
+      $(this).text(lang[$(this).data('key')]);
+    })
+  });
+});

693052284d2081cfbbe1aa860c03e6ee8151aeab
Jackieyewang <306981258@qq.com>
2021-11-19 01:45:48 +0800

    change some UI

--- language.js
+++ language.js
@@ -4,6 +4,7 @@ var translations = {
     'intro2':'togETHer is an Ethereum community alliance committed to driving the ecosystem foward. We connect community resources to collaborate on Ethereum blockchain documentation, development, research and events.',
     'connect-btn':'CONTACT US',
     'members':'Our Members.',
+    'expert':'Expert Members.',
     'intromember':'Together\'s current membership includes EThPlanet, ECN, EthFans, DAOSquare, PrimitivesLane, Hiblock, Zhixiong Pan, Rebase and BTCU.',
     'introETHplanet':'ETHPlanet — Building a community to engage, connect, and empower builders. ETHPlanet, a planet where you can connect with all the developers, dapp founders, digital artists, investors, and other innovators in the field from around the Ethereum world.',
     'introECN':'ECN as an Ethereum Community Network, is committed to facilitating the adoption of Ethereum and blockchain and further bridging the Chinese-speaking community with global Ethereum ecosystem. We’re making fundamental efforts including education, documentation, community and events.',
@@ -12,8 +13,8 @@ var translations = {
     'primitiveslane':'Primitives Lane',
     'introprimitiveslane':'Primitives Lane (formerly known as Shanghai Advanced Tech Seminar) is a non-profit community focusing on cutting-edge technology, dedicated to exploring the development path of underlying technologies and researching fundamental public issues. By providing a public technology discussion platform for the industry through offline seminars and subject research, it promotes in-depth communication within the industry and enhances the influence of the industry in society. Currently, Primitives Lane focuses on blockchain, cryptography and other related areas.',
     'introHiblock':'HiBlock is a developer community focused on blockchain, we not only promote blockchain among developers, but also help developers really master blockchain technology and applications.',
-    'introRebase':'Rebase community is committed to helping geeks make better use of technological innovation and promote social change.Rebase community project introduction: 1. Web3 Daily: Daily reading for geeks. A Web3 related technical article is published every day, and each article includes three recommendations. The recommended content comes from the members of the Web3 Daily Think Tank, and the content is collected, sorted out and published by community volunteers.	2. Rebase University: A place where geeks self-organized learning, advocating learning and sharing. Committed to building an open knowledge society. 3. Rostra: Inspired by Rebase University, Rostra is committed to changing the way people learn, research and share. Researchers publish research plans and raise funds from the society to fund their own research. The benefits of research results can benefit funders. 4. Dapp Learning: The project vision is to give junior developers an executable and simplest blockchain Dapp learning roadmap, and to give advanced developers a platform for communication and collaboration. The project is deeply integrated with Rebase University. A study group is set up to divide tasks and then study mainstream dapp projects together. Technology sharing is carried out every week. The sharing videos will be uploaded to the Rebase account at Bilibili. 5. Who is hiring: In order to facilitate the team\'s recruitment of developers, we setup a repo on github for freely publishing work requirements. 6. Rebase D. Ventures: Rebase D. Ventures is a community venture capital fund originating from the Rebase community. It uses funds from the community to realize the vision of helping and accomplishing blockchain technology teams and projects.',
-    'introBTCU':'The Blockchain Technology Community of Universities is composed of students from Tsinghua University, Peking University, Zhejiang University, Shanghai Jiaotong University, Fudan University, Renmin University of China, Central University of Finance and Economics, Wuhan University, Huazhong University of Science and Technology, Sichuan University, etc. The heads of the Blockchain Association of 20 Chinese universities jointly initiated the establishment of a cooperative community for blockchain technology learning, education and industrial communications among universities. It is a non-profit, unofficial, unincorporated, and spontaneous community.',
+    'introRebase':'Rebase community is committed to helping geeks make better use of technological innovation and promote social change. Rebase community project introduction: 1. Web3 Daily: Daily reading for geeks. A Web3 related technical article is published every day, and each article includes three recommendations. The recommended content comes from the members of the Web3 Daily Think Tank, and the content is collected, sorted out and published by community volunteers. 2. Rebase University: A place where geeks self-organized learning, advocating learning and sharing. Committed to building an open knowledge society. 3. ...',
+    'introBTCU':'The Blockchain Technology Community of Universities is composed of students from Tsinghua University, Peking University, Zhejiang University, Shanghai Jiaotong University, Fudan University, Renmin University of China, Central University of Finance and Economics, Wuhan University, Huazhong University of Science and Technology, Sichuan University, etc. The heads of the Blockchain Association of 20 Chinese universities jointly initiated the establishment of a cooperative community for blockchain technology learning, education and industrial communications among universities. It is a non-profit....',
     'Pan':'Zhixiong Pan',
     'introPan':'ChainNews | Director of Research Observer of Ethereum/Layer2/Web3/DeFi.',
     'events':'Events',
@@ -27,7 +28,7 @@ var translations = {
     'how2join':'HOW TO BECOME ONE OF US',
     'introJoin':'Ethereum contributors (community/individuals) are embraced by togETHer.',
     'wanna0':'Wanna join us?',
-    'submit0':'Submit your proposal by Github issue',
+    'submit0':'Submit your proposal by Github ',
 
 
     /* ... */
@@ -37,6 +38,7 @@ var translations = {
     'intro2':'togETHer是一个致力于推动以太坊生态向前发展的以太坊社区联盟。我们连接社区资源，一起合协作以太坊区文档、开发、研究和活动。',
     'connect-btn':'联系我们',
     'members':'我们的成员',
+    'expert':'专家成员',
     'intromember':'Together的成员包括EThPlanet, ECN, EthFans, DAOSquare, PrimitivesLane, Hiblock, Zhixiong Pan, Rebase and BTCU.',
     'introETHplanet':'ETHPlanet 以太行星 — 共建、互助、互联，赋能以太坊开发者社群ETHPlanet是一颗汇聚来自世界各地的以太坊翘楚的神奇星球，在这颗星球上，您可以与开发者、dapp 创始人、数字艺术家、投资者以及其他领域的创新者进行联系。我们携手支持并推进以太坊生态系统的开发。',
     'introECN':'ECN 作为以太坊社区网络，旨在以社区化合作方式推动以太坊和区块链应用，进一步桥接华语社区和全球以太坊生态。我们为以太坊爱好者和开发者提供全面的教育支持、优质的前沿资讯和开放的社区平台。',
@@ -60,7 +62,7 @@ var translations = {
     'how2join':'如何成为我们的一员',
     'introJoin':'together欢迎每一个以太坊贡献者(社区/个人)。',
     'wanna0':'想加入我们吗?',
-    'submit0':'提交你的proposal在 Github issue',
+    'submit0':'提交你的proposal在 Github ',
     /* ... */
   }
 };
